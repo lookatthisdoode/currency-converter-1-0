c5d21a46df1c9fcc35421c98835c68c2aa880302
andrei <andruhahoke@gmail.com>
2024-01-24 19:28:10 +0100

    changed logic of some elements

--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -1,11 +1,19 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { changeFromCurrency } from '../state/store'
 import CurrencyInput from './CurrencyInput'
-import LeftCurrencyPicker from './LeftCurrencyPicker'
+import CurrencyPicker from './CurrencyPicker'
 
 const LeftSide = () => {
+  const dispatch = useDispatch()
+
+  // Define action to pass into picker (make picker reusable)
+  const action = (payload) => dispatch(changeFromCurrency(payload))
+  const currentCurrency = useSelector((state) => state.fromCurrency)
+
   return (
     <div className="side-wrapper overpass">
       <h3>I have</h3>
-      <LeftCurrencyPicker />
+      <CurrencyPicker action={action} currentCurrency={currentCurrency} />
       <CurrencyInput />
     </div>
   )
--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -1,11 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux'
 import CurrencyOutput from './CurrencyOutput'
-import RightCurrencyPicker from './RightCurrencyPicker'
+import CurrencyPicker from './CurrencyPicker'
+import { changeToCurrency } from '../state/store'
 
 const RightSide = () => {
+  const dispatch = useDispatch()
+
+  // Define action to pass into picker (make picker reusable)
+  const currentCurrency = useSelector((state) => state.toCurrency)
+  const action = (payload) => dispatch(changeToCurrency(payload))
+
   return (
     <div className="side-wrapper overpass">
-      <h3 className="">I will recieve</h3>
-      <RightCurrencyPicker />
+      <h3>I will recieve</h3>
+      <CurrencyPicker action={action} currentCurrency={currentCurrency} />
       <CurrencyOutput />
     </div>
   )
