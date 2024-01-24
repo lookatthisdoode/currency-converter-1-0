0e4d66276ab93e83a2ba4e8951ffb5d927fbf270
andrei <andruhahoke@gmail.com>
2024-01-24 19:26:00 +0100

    added external config file

---
+++ converter.config.js
@@ -0,0 +1,11 @@
+// List of all available currencies:
+
+//    'AUD', 'GBP', 'CNY',
+//    'DKK', 'EUR', 'JPY',
+//    'CAD', 'HUF', 'NOK',
+//    'PLN', 'RON', 'SEK',
+//    'CHF', 'USD'
+
+const selectedCurrenciesList = ['EUR', 'GBP', 'CAD', 'PLN', 'JPY', 'HUF']
+
+export default selectedCurrenciesList
